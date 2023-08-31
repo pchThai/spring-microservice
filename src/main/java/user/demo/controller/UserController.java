9321778cd84b132cc9919550b3085f41c8116ac9
Phạm Châu Hồng Thái <88072404+pchThai@users.noreply.github.com>
2023-08-31 12:24:12 +0700

    Init Project

---
+++ src/main/java/user/demo/controller/UserController.java
@@ -0,0 +1,7 @@
+package user.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
---
+++ src/main/java/user/demo/entity/User.java
@@ -0,0 +1,25 @@
+package user.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name ="user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+}
